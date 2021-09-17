94e954fb15215aaac02581940e9da4a872b58e1b
quynhnn-vn <nnquynh.0111@gmail.com>
2021-09-17 17:13:36 +0200

    Update weather-api to fetch weather data by lat/lon

--- src/datasources/weather-api.js
+++ src/datasources/weather-api.js
@@ -7,10 +7,10 @@ class WeatherAPI extends RESTDataSource {
     this.baseURL = "https://api.openweathermap.org/data/2.5/";
   }
   // Perform a GET request to the "/tracks" endpoint
-  getWeather(location) {
+  getWeather(location, lat, lon) {
     return this.get(
-      `forecast?q=${location},fr?lang=fr&appid=${openweatherKey}`
-    );
-  }
+      `forecast?q=${location}&lat=${lat}&lon=${lon}&lang=fr&units=metric&appid=${openweatherKey}`
+      );
+    }
 }
 module.exports = WeatherAPI;
--- src/resolvers.js
+++ src/resolvers.js
@@ -7,9 +7,9 @@ const resolvers = {
     articles: (_, { location }, { dataSources }) => {
       return dataSources.articleAPI.getArticles(location);
     },
-    weather: (_, { location }, { dataSources }) => {
-      return dataSources.weatherAPI.getWeather(location);
-    }
+    weather: (_, { location, lat, lon }, { dataSources }) => {
+      return dataSources.weatherAPI.getWeather(location, lat, lon);
+    },
   },
 };
 module.exports = resolvers;
